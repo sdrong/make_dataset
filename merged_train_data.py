d8e65e1939db794a20e88ec0998456460bed1af0
choizeus02 <choizeus0212@gmail.com>
2024-05-08 02:22:29 +0900

    2024 05 08 augmentation & all ( 벤치마크 제외)

--- merged_train_data.py
+++ merged_train_data.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 # 파일 리스트
-file_list = ['dataset/1.jsonl','dataset/2.jsonl','dataset/3.jsonl']
+file_list = ['dataset/cpu_amd_merged_train_data.jsonl','dataset/cpu_Intel_merged_train_data.jsonl','dataset/gpu_merged.jsonl','dataset/gpu_qna_merged.jsonl','dataset/motherBoard_merged.jsonl','dataset/power.jsonl','dataset/qna_augmented.jsonl','dataset/ram_merged.jsonl','dataset/ssd_merged.jsonl',]
 
 # 새로운 파일 경로
-output_file_path = 'dataset/qna_data_merged.jsonl'
+output_file_path = 'dataset/all.jsonl'
 
 # 모든 파일을 읽고 새 파일에 내용을 쓰기
 with open(output_file_path, 'w') as outfile:
